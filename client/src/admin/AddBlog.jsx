4cdc5c9cb51c611281d2b18d66f8dd15379ec4e9
Rambali5550 <rambali.lodha@xtranetindia.com>
2023-07-20 13:27:13 +0530

    console Change

--- client/src/admin/AddBlog.jsx
+++ client/src/admin/AddBlog.jsx
@@ -28,7 +28,7 @@ const AddBlog = () => {
             },
           }
         );
-        console.log("Data Post Successfullyyyyyy");
+        console.log("Data Post Successfully Hello I am ");
       }
     } catch (error) {
       console.log("something error in axios and url --->", error);
